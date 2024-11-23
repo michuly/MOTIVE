265df5cf64d43cd73e3edb224c86d24d75c86c82
michuly <michalshaham@mail.tau.ac.il>
2024-11-23 12:29:48 -1000

    sst

--- scripts/SST.py
+++ scripts/SST.py
@@ -10,16 +10,17 @@ get two plots:
 ### get history file names
 min_num, max_num = 141743-24*1, 141743+24*1
 his_files, tot_depths, time_dim = get_concatenate_parameters(min_num, max_num)
-depths = tot_depths
 ### save an empty psd file ###
-dst_path_v = os.path.join(data_path_his, "v_1N_daily_avg.nc")
-print('Saving v into data file:', dst_path_v)
+dst_path = os.path.join(data_path_his, "SST_daily_avg.nc")
+print('Saving v into data file:', dst_path)
 
 with Dataset(os.path.join(grd_path, grd_name)) as dat_grd:
     if to_slice:
-        lon_array = dat_grd.variables['lon_rho'][lat_ind_1N, min_xi_rho:max_xi_rho+1]
+        lon_array = dat_grd.variables['lon_rho'][lat_ind, min_xi_rho:max_xi_rho+1]
+        lat_array = dat_grd.variables['lon_rho'][min_eta_rho:max_eta_rho+1, lon_ind]
     else:
         lon_array = dat_grd.variables['lon_rho'][lat_ind_1N, :]
+        lat_array = dat_grd.variables['lon_rho'][:, lon_ind]
 
 ### concatenate time to one series ###
 time_jump = 1
@@ -30,8 +31,8 @@ else:
 time_size = time_step * len(his_files)
 print("Time parameters: ", time_size, time_dim, time_step, time_jump)
 ind_time = 0
-v = np.zeros((time_size, 88, len_xi_rho))
-v.fill(np.nan)
+sst = np.zeros((time_size, len_eta_rho, len_xi_rho))
+sst.fill(np.nan)
 ocean_time = np.zeros(time_size)
 ocean_time.fill(np.nan)
 for i in range(len(his_files)):
@@ -40,44 +41,36 @@ for i in range(len(his_files)):
     sys.stdout.flush()
     dat_his = Dataset(his_file, 'r')
     if to_slice:  # Shape: time, depth, y, x?e
-        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][::time_jump, :, lat_ind_1N, min_xi_rho:max_xi_rho+1]
+        sst[ind_time:(ind_time + time_step), :, :] = dat_his.variables['temp'][::time_jump, 0, min_eta_rho:max_eta_rho + 1, min_xi_rho:max_xi_rho + 1]
     else:
-        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][::time_jump, :, lat_ind_1N, :]
+        sst[ind_time:(ind_time + time_step), :, :] = dat_his.variables['temp'][::time_jump, 0, :, :]
     ocean_time[ind_time:(ind_time+time_step)] = dat_his.variables['ocean_time'][:]
     dat_his.close()
     ind_time = ind_time + time_step
 
-print('Calculating gradient...')
-sys.stdout.flush()
-dvz = np.gradient(v,depths,axis=1)
-print('Check dimensions: ', lon_array.shape, len_xi_rho, v.shape)
-sys.stdout.flush()
+print('Check dimensions: ', len_eta_rho, len_xi_rho, sst.shape)
 
-n_chunks = v.shape[0] // 24
-v = v[:n_chunks * 24, :, :]
-v=v.reshape(-1, 24, v.shape[1], v.shape[2]).mean(axis=1)
-dvz = dvz[:n_chunks * 24, :, :]
-dvz=dvz.reshape(-1, 24, v.shape[1], v.shape[2]).mean(axis=1)
+n_chunks = sst.shape[0] // 24
+sst = sst[:n_chunks * 24, :, :]
+sst=sst.reshape(-1, 24, sst.shape[1], sst.shape[2]).mean(axis=1)
 ocean_time = ocean_time[:n_chunks * 24][::24]
-print('Check dimensions: ', v.shape, dvz.shape, ocean_time.shape)
+print('Check dimensions: ', sst.shape, ocean_time.shape)
 
-print('Saving 1N meridional velocity...')
+print('Saving SST...')
 sys.stdout.flush()
 # if not os.path.exists(dst_path):
-dat_dst = Dataset(dst_path_v, 'w')
-dat_dst.createDimension('depths', len(tot_depths))
-dat_dst.createVariable('depths', np.dtype('float32').char, ('depths',))
-dat_dst.variables['depths'][:] = tot_depths
+dat_dst = Dataset(dst_path, 'w')
+dat_dst.createDimension('lat', len(lat_array))
+dat_dst.createVariable('lat', np.dtype('float32').char, ('lat',))
+dat_dst.variables['lat'][:] = lat_array
 dat_dst.createDimension('lon', len_xi_rho)
 dat_dst.createVariable('lon', np.dtype('float32').char, ('lon',))
 dat_dst.variables['lon'][:] = lon_array
 dat_dst.createDimension('ocean_time', len(ocean_time))
 dat_dst.createVariable('ocean_time', np.dtype('float32').char, ('ocean_time',))
 dat_dst.variables['ocean_time'][:] = ocean_time
-dat_dst.createVariable('v', np.dtype('float32').char, ('ocean_time','depths','lon'))
-dat_dst.variables['v'][:] = v
-dat_dst.createVariable('dvz', np.dtype('float32').char, ('ocean_time','depths','lon'))
-dat_dst.variables['dvz'][:] = dvz
+dat_dst.createVariable(sst, np.dtype('float32').char, ('ocean_time','lat','lon'))
+dat_dst.variables[sst][:] = sst
 dat_dst.close()
-print('DONE: saved v to data file ', dst_path_v)
+print('DONE: saved SST to data file ', dst_path)
 sys.stdout.flush()
