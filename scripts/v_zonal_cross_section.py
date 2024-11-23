2d92dd80860776392d3505da70a354994c8799ad
michuly <michalshaham@mail.tau.ac.il>
2024-11-22 23:24:24 -1000

    meridional velocity and longer psd

--- scripts/v_zonal_cross_section.py
+++ scripts/v_zonal_cross_section.py
@@ -22,9 +22,14 @@ with Dataset(os.path.join(grd_path, grd_name)) as dat_grd:
         lon_array = dat_grd.variables['lon_rho'][lat_ind_1N, :]
 
 ### concatenate time to one series ###
-time_step = 12
-ind_time = 0
+time_jump = 1
+if time_jump > 1:
+    time_step = int(np.floor(time_dim / time_jump))
+else:
+    time_step = time_dim
 time_size = time_step * len(his_files)
+print("Time parameters: ", time_size, time_dim, time_step, time_jump)
+ind_time = 0
 v = np.zeros((time_size, 88, len_xi_rho))
 v.fill(np.nan)
 ocean_time = np.zeros(time_size)
@@ -35,9 +40,9 @@ for i in range(len(his_files)):
     sys.stdout.flush()
     dat_his = Dataset(his_file, 'r')
     if to_slice:  # Shape: time, depth, y, x?e
-        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][:, :, lat_ind_1N, min_xi_rho:max_xi_rho+1]
+        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][::time_jump, :, lat_ind_1N, min_xi_rho:max_xi_rho+1]
     else:
-        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][:, :, lat_ind_1N, :]
+        v[ind_time:(ind_time + time_step), :, :] = dat_his.variables['v'][::time_jump, :, lat_ind_1N, :]
     ocean_time[ind_time:(ind_time+time_step)] = dat_his.variables['ocean_time'][:]
     dat_his.close()
     ind_time = ind_time + time_step
@@ -45,6 +50,12 @@ for i in range(len(his_files)):
 print('Calculating gradient...')
 dvz = np.gradient(v,depths,axis=1)
 print('Check dimensions: ', lon_array.shape, len_xi_rho, v.shape)
+
+n_chunks = v.shape[0] // 24
+v = v[:n_chunks * 24, :, :]
+v=v.reshape(-1, 24, v.shape[1], v.shape[2]).mean(axis=1)
+dvz = dvz[:n_chunks * 24, :, :]
+dvz=dvz.reshape(-1, 24, v.shape[1], v.shape[2]).mean(axis=1)
 
 print('Saving 1N meridional velocity...')
 # if not os.path.exists(dst_path):
@@ -55,9 +66,9 @@ dat_dst.variables['depths'][:] = tot_depths
 dat_dst.createDimension('lon', len_xi_rho)
 dat_dst.createVariable('lon', np.dtype('float32').char, ('lon',))
 dat_dst.variables['lon'][:] = lon_array
-dat_dst.createDimension('ocean_time', time_size)
+dat_dst.createDimension('ocean_time', time_size//24)
 dat_dst.createVariable('ocean_time', np.dtype('float32').char, ('ocean_time',))
-dat_dst.variables['ocean_time'][:] = ocean_time
+dat_dst.variables['ocean_time'][:] = ocean_time[::,24]
 dat_dst.createVariable('v', np.dtype('float32').char, ('ocean_time','depths','lon'))
 dat_dst.variables['v'][:] = v
 dat_dst.createVariable('dvz', np.dtype('float32').char, ('ocean_time','depths','lon'))
